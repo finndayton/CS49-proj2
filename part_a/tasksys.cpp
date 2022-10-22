47ba3e9bb02c4b9764f80d32883849cbfd4c9ad1
shashank2000 <sr22@stanford.edu>
2022-10-22 09:10:15 -0700

    temp workaround

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -290,7 +290,7 @@ void TaskSystemParallelThreadPoolSleeping::makeThreadPool() {
 void TaskSystemParallelThreadPoolSleeping::killThreadPool() {
     // don't need the lock, just notify all so they can exit
     // might need to do some gymnastics with the lock
-    condition_variable->notify_all();
+    condition_variable_->notify_all();
     for (int i = 0; i < max_threads; i++) {
         // make threads, and make them free to start off with
         workers[i].join();   
