713545da1abd78eb4f5ea904ff18faaf174690fd
shashank2000 <sr22@stanford.edu>
2022-10-22 08:51:04 -0700

    temp workaround

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -306,7 +306,7 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
         // lock to check task_queue size
         mutex_->lock();
         printf("task queue size is %d\n", task_queue.size());
-        printf("busy threads is %d\n", busy_threads);
+        printf("busy threads is %d\n", busy_threads.load());
         if (task_queue.size() == 0 && busy_threads == 0) {
             // no more work to be done, return from run
             mutex_->unlock();
